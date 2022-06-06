09266e0d13903300170610e676a70df56eb5eb22
taekkii <ohjtgood@gmail.com>
2022-06-06 14:54:32 +0900

    move to demeter

--- src/task/nerf/train.py
+++ src/task/nerf/train.py
@@ -4,20 +4,29 @@ from . import ray
 from . import render
 
 
+def squared_error(c_gt,c_render):
+    return ((c_gt-c_render)**2).sum()
 class NeRFTrainer(trainer.Trainer):
-    
+
     #override
     def start(self):
-        #override
-        def start(self):
-            pass
+        pass
 
-        #override
-        def step(self,data):
-            
-            pass
+    #override
+    def step(self,data):
+        train_chunk = self.trainchunks[0] # temporary
+
+        gt_imgs,poses = data
+        gt_imgs = gt_imgs.to(self.settings['device'])
+        poses   = poses.to(self.settings['device'])
         
-        def validate(self):
+        
+        rendered_imgs, mse_loss = train_chunk.forward_and_backward(poses , target=gt_imgs , obj_func=squared_error)
+        
+        
 
-            pass
-            
+        
+    
+    def validate(self):
+        pass
+        
